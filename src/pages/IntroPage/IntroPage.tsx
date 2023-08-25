f1b5e025b1db6f6656104e810ba4a21510778144
vinyl810 <19thsummer@kaist.ac.kr>
2023-08-26 01:21:27 +0900

    fix: fix vh

--- src/pages/IntroPage/IntroPage.tsx
+++ src/pages/IntroPage/IntroPage.tsx
@@ -1,9 +1,9 @@
 export const IntroPage = () => {
   return (
-    <div className="first-page">
-      <div className="border"></div>
+    <div className="first-page" style={{height: window.innerHeight - 30}}>
+      <div className="border" style={{height: window.innerHeight - 30}}></div>
       <div className="big-logo"></div>
       <div className="text-title"></div>
     </div>
-  )
+  );
 }
