ac67ff364108e4bc4c8bb8a7a1fb6b699a9c535d
vinyl810 <19thsummer@kaist.ac.kr>
2023-08-25 13:28:35 +0000

    initial commit

---
+++ src/pages/IntroPage/IntroPage.tsx
@@ -0,0 +1,9 @@
+export const IntroPage = () => {
+  return (
+    <div className="first-page">
+      <div className="border"></div>
+      <div className="big-logo"></div>
+      <div className="text-title"></div>
+    </div>
+  )
+}
