fcb3026dd39bf354bfef3fe8da0f7275ef03db42
vinyl810 <19thsummer@kaist.ac.kr>
2023-09-04 14:20:06 +0000

    implement cast, numbers

--- src/pages/IntroPage/DetailContent.tsx
+++ src/pages/IntroPage/DetailContent.tsx
@@ -1,5 +1,6 @@
-import './style.css'
+import './detail.css'
 import {contentData} from '../../content'
+import * as React from 'react'
 
 export interface DetailContentProp {
   showDetail: string | null | 'init'
@@ -7,6 +8,8 @@ export interface DetailContentProp {
 
 export const DetailContent = (props: DetailContentProp) => {
   const {showDetail} = props
+
+  const [showPopup, setShowPopup] = React.useState<string | null>(null)
 
   const getTitle = (showDetail: string | null | 'init') => {
     switch (showDetail) {
@@ -23,14 +26,55 @@ export const DetailContent = (props: DetailContentProp) => {
     }
   }
 
+  const getContent = (showDetail: string | null | 'init') => {
+    switch (showDetail) {
+      case 'cast':
+        return contentData.cast.map(x => {
+          return (
+            <ImageItem 
+              onClick={() => setShowPopup(x.url)}
+              url={`profiles/${x.url}.png`} 
+              style={x.style}
+            />
+          )
+        })
+      case 'numbers':
+        return contentData.numbers.map(x => {
+          return (<div className="number-item">
+            <div className='number-head'>{x.head}</div>
+            <div className='number-title'>{x.title}</div>
+            <div className='number-info'>{x.info}</div>
+          </div>)
+        })
+      case 'synopsis':
+        return contentData.synopsis.map(x => <div className={`detail-sentence ${x.style}`}>{x.text}</div>)
+      case 'patron':
+      default:
+        return '아직 후원 정보가 준비되지 않았습니다. 조금만 기다려주세요!'
+    }
+  }
+
   return (
     <div className="detail-content">
+      {showPopup && <ImagePopup url={showPopup} onClick={() => {setShowPopup(null)}} />}
       <div className="detail-title">
         {getTitle(showDetail)}
       </div>
       <div className="detail-text">
-        {contentData.synopsis.map(x => <div className="detail-sentence">{x.text}</div>)}
+        {getContent(showDetail)}
       </div>
     </div>
   );
+}
+
+const ImageItem = (props: {url: string, style: string, onClick: () => void}) => {
+  return <img onClick={props.onClick} className={`detail-image ${props.style}`} src={props.url} alt={`profile${props.url}`}/>
+}
+
+const ImagePopup = (props: {url: string, onClick: () => void}) => {
+  return (
+    <div className="popup-wrapper">
+      <img onClick={props.onClick} src={`profiles/${props.url}.png`} alt={`profile${props.url}`} className="image-popup normal" />
+    </div>
+  )
 }
